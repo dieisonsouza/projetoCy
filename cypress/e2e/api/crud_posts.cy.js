f185c6fc389b6ff3ae94f510f4e9c28fca680908
dieisonsouza <dieisonss.1986@gmail.com>
2022-12-27 20:06:04 +0000

    Api - Post e Get

--- cypress/e2e/api/crud_posts.cy.js
+++ cypress/e2e/api/crud_posts.cy.js
@@ -1,18 +1,47 @@
 describe('CRUD - Posts', () => {
+
+    let postId = ''
+    let mensagem = 'Este post foi feito pelo Cypress1'
     
     before(() => {
 
         cy.request({
             method: 'POST',
-            url: '/apa/auth',
+            url: '/api/auth',
             body: {
-                email: 'testeIterasys@iterasys.com',
-                password: '123456'
+                email: Cypress.env('email'),
+                password: Cypress.env('password')
             }
+        }).then(() => {
+            Cypress.Cookies.defaults({
+                preserve: 'jwt'
+            })
         })
     })
 
-    it('teste', () => {
-        cy.log('Teste')
+    it('cria um post', () => {
+        
+        cy.request({
+            method: 'POST',
+            url: '/api/posts',
+            body: {
+                text: mensagem
+            }
+        }).then(({ status, body }) => {
+            expect(status).to.eq(201)
+            expect(body.text).to.eq(mensagem)
+            postId = body._id
+        })
+    })
+
+    it('ler o post', () => {
+
+        cy.request({
+            method: 'GET',
+            url: `/api/posts/${postId}`
+        }).then(({ status, body }) => {
+            expect(status).to.eq(200)
+            expect(body.text).to.eq(mensagem)
+        })
     })
 })
